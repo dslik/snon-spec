847dad5b3b4d9cb217c347ab7fefee8eb4baff39
David Slik <dslik@acm.org>
2026-04-15 00:00:08 -0700

    Updated tests to extract and validate spec examples

--- tests/schema_validate.rb
+++ tests/schema_validate.rb
@@ -1,6 +1,5 @@
-require "time"
-require "json"
 require "optparse"
+require "json"
 require "json-schema"
 
 # ------------------------------------------------------------------------------
@@ -10,15 +9,20 @@ options = {}
 optparse = OptionParser.new do|opts|
 	opts.banner = "Usage: schema_validate.rb schema input"
 
-	options[:schema] = "snon-schema.json"
+	options[:schema] = "../v4/snon-schema.json"
 	opts.on( '-s', '--schema file', 'Path to schema file to use for validation' ) do |schema|
 		options[:schema] = schema
 	end
 
-	options[:input] = "input.json"
 	opts.on( '-i', '--input file', 'Path to input file to validate' ) do |input|
 		options[:input] = input
 	end
+
+	options[:spec] = "../index.html"
+	opts.on( '-spec', '--spec file', 'Path to specification to validate' ) do |spec|
+		options[:spec] = spec
+	end
+
 
 	opts.on( '-h', '--help', 'Display command line help information screen' ) do
 		puts opts
@@ -29,20 +33,73 @@ end
 optparse.parse!
 
 # ------------------------------------------------------------------------------
-# Load inputs
+# Load schema
 # ------------------------------------------------------------------------------
 schema_file = File.read(options[:schema])
-input_file = File.read(options[:input])
-
 schema_json = JSON::parse(schema_file)
-input_json = JSON::parse(input_file)
 
 # ------------------------------------------------------------------------------
-# Validate the schema
+# Validate specified input file or the full SNON spec
 # ------------------------------------------------------------------------------
-validation_result = JSON::Validator.fully_validate(schema_json, input_json)  
+if(options[:input])
+	input_file = File.read(options[:input])
+	input_json = JSON::parse(input_file)
+	validation_result = JSON::Validator.fully_validate(schema_json, input_json)
 
-validation_result.each do |line|
-	print line
+	validation_result.each do |line|
+		print line
+	end
+	
+	if(validation_result.count == 0)
+		print "No validation errors found"
+	end
+else
+	# Extract examples
+	skip = false
+	start = true
+	count = 0
+	snon_lines = Array.new
+
+	File.foreach(options[:spec]) do |line|
+		if(line.index("    ") == 0)
+			if(start == true)
+				# Beginning of code cite
+				start = false
+
+				if(line == "    [\n")
+					# Beginning of SNON fragment
+					skip = false
+					count = count + 1
+				else
+					skip = true
+				end
+			end
+			
+			if(skip == false)
+				snon_lines << line
+			end
+		else
+			if(snon_lines.count != 0)
+				# Validate SNON fragment
+				print "\n--------------------------------------\n"
+				print "Validating Example #{count}:\n"
+				input_json = JSON::parse(snon_lines.join)
+				validation_result = JSON::Validator.fully_validate(schema_json, input_json)
+
+				validation_result.each do |line|
+					print line
+				end
+
+				if(validation_result.count == 0)
+					print "No validation errors found"
+				end
+			end
+
+			snon_lines.clear
+
+			start = true
+		end
+	end
 end
+
 print "\n"
